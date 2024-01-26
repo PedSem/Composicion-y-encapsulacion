e45fab7cd9dafb392c61257643d590fe9b50ae2b
PedSem <153596408+PedSem@users.noreply.github.com>
2024-01-26 13:34:39 +0100

    Create MainEncapsulacion.java

---
+++ MainEncapsulacion.java
@@ -0,0 +1,10 @@
+public class MainEncapsulacion {
+    public static void main(String[]args){
+
+        Impresora impresora=new Impresora(6,true);
+        System.out.println("Cantidad de toner:"+ impresora.addcantidad(5));
+        System.out.println("Páginas de doblecara:"+ impresora.imprimirpaginas(1));
+
+
+    }
+}
